8c431ec03a71a0755c63831b67a1df2e82800658
getty104 <hayabusatoshihumi@gmail.com>
2018-03-31 22:12:38 +0900

    fix gemspec

--- lib/csql.rb
+++ lib/csql.rb
@@ -1,5 +1,43 @@
 require "csql/version"
+require 'open3'
+require 'csv'
+require 'sql-parser'
 
-module Csql
-  # Your code goes here...
+module CSQL
+  class SQL
+    def initialize(filepath)
+      @parser = SQLParser::Parser.new
+      @filepath = filepath
+    end
+
+    def execute query
+      begin
+        modified_query = query.gsub(/csvfile/, @filepath)
+        result,err,process = Open3.capture3("q -H -d \',\' \'#{modified_query}\'")
+        if err != ""
+          raise CSQLException.new(err)
+        end
+      end
+      ast = @parser.scan_str(query)
+      column = ast.query_expression.list.to_sql
+      columns = nil
+      if column == "*"
+        columns = CSV.table(@filepath).headers.map(&:to_s)
+      else
+        columns = column.chomp.split(',').map{|c|c.chomp}
+      end
+      result = result.chomp.split("\n").map!{|r|
+        data = r.split(",")
+        hash = Hash.new
+        data.size.times do |i|
+          hash[columns[i]] = data[i]
+        end
+        return hash
+      }
+      return result
+    end
+  end
+
+  class CSQLException < Exception
+  end
 end
